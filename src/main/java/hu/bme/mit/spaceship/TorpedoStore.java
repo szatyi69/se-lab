1f28160049066d0684ba838949ce881b1a282a40
unknown <184899@BME.HU>
2024-10-04 13:43:13 +0200

    Updated to meet pull request

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -32,7 +32,7 @@ public class TorpedoStore {
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      throw new IllegalArgumentException("numberOfTorpedos");
+      throw new IllegalArgumentException("numberOfTorpedos"); // Köszi a kommentet! Don't fix what's aint broken.
     }
 
     boolean success = false;
